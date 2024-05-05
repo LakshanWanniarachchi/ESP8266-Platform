7061fff5144996b906bccbcaf2d3363effc43c8c
Lakshan Lakshitha <134108423+LakshanWanniarachchi@users.noreply.github.com>
2024-05-05 20:52:30 +0530

    create cors

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ESP32'
-   
+    
+    CORS(app)
+
     
     from .views import views 
    
